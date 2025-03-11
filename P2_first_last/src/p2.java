1a05b3cc5ee8aa97ea4f5e0752875bbead4a59e3
1903438 <1903438@01-18104.pusd.dom>
2025-03-11 14:08:44 -0700

    Hi Mr.David

--- P2_first_last/src/p2.java
+++ P2_first_last/src/p2.java
@@ -1,9 +1,51 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class p2 {
 
+	static boolean Stack = false;
+	static boolean Queue = false;
+	static boolean Opt = false;
+	static boolean Time = false;
+	static boolean Incoordinate = false;
+	static boolean Outcoordinate = false;
+	static boolean Help = false;
+
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("p2");
+	
+		
 	}
-
+	
+	public static void readMap(String filename) {
+		
+		try {
+			File file = new File(filename);
+			Scanner scanner = new Scanner(file);
+			
+			int numRows = scanner.nextInt();
+			int numCols = scanner.nextInt();
+			int bumRooms = scanner.nextInt();
+			
+			int rowIndex = 0;
+			
+			while(scanner.hasNextLine()) {
+				String row = scanner.nextLine();
+				
+				if(row.length() > 0) {
+					for(int i = 0; i < numCols && i < row.length(); i++) {
+						char el = row.charAt(i);
+						Tile obj = new Tile(rowIndex, i , el );
+					}
+				}
+			}
+			
+			
+		} catch (FileNotFoundException e) {
+			System.out.println(e);
+		}
+	}
+	
 }
