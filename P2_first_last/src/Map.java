27d3eb7411801038ca313fe5a546e415e9cd87ce
1903438 <1903438@01-18104.pusd.dom>
2025-03-13 14:11:52 -0700

    Newer map code

--- P2_first_last/src/Map.java
+++ P2_first_last/src/Map.java
@@ -3,44 +3,14 @@ public class Map {
 	
 	private Tile[][] map;
 	private int row, col,rooms;
-	private char type;
-	public Tile[][] getMap() {
-		return map;
-	}
-	public void setMap(Tile[][] map) {
-		this.map = map;
-	}
-	public int getRow() {
-		return row;
-	}
-	public void setRow(int row) {
-		this.row = row;
-	}
-	public int getCol() {
-		return col;
-	}
-	public void setCol(int col) {
-		this.col = col;
-	}
-	public int getRooms() {
-		return rooms;
-	}
-	public void setRooms(int rooms) {
-		this.rooms = rooms;
-	}
-	public char getType() {
-		return type;
-	}
-	public void setType(char type) {
-		this.type = type;
-	}
-	public Map(Tile[][] map, int row, int col, int rooms, char type) {
+
+	public Map(Tile[][] map, int row, int col, int rooms) {
 		super();
 		this.map = map;
 		this.row = row;
 		this.col = col;
 		this.rooms = rooms;
-		this.type = type;
+	
 	}
 	
 }
